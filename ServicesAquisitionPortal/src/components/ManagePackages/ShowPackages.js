501f7f0c69ce77ce387cbb51086708f3f2621a62
Usman Khan <muhammadusmankhan307@gmail.com>
2019-07-17 20:31:35 +0500

    usersite--home page.

--- ServicesAquisitionPortal/src/components/ManagePackages/ShowPackages.js
+++ ServicesAquisitionPortal/src/components/ManagePackages/ShowPackages.js
@@ -1,200 +1,148 @@
 import React from 'react';
-import UserAppBar from '../Layouts/appBar.js';
+import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import StarIcon from '@material-ui/icons/StarBorder';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
-import clientSideBG1 from '../img/clientSideBG1.jpg';
+import Link from '@material-ui/core/Link';
 
+function MadeWithLove() {
+	return (
+		<Typography variant="body2" color="textSecondary" align="center">
+			{'Built with love by the '}
+			<Link color="inherit" href="https://material-ui.com/">
+				Material-UI
+			</Link>
+			{' team.'}
+		</Typography>
+	);
+}
 const useStyles = makeStyles(theme => ({
-	'@global': {
-		body: {
-			backgroundColor: theme.palette.common.white,
-		},
-		ul: {
-			margin: 0,
-			padding: 0,
-		},
-		li: {
-			listStyle: 'none',
-		},
-	},
-	appBar: {
-		borderBottom: `1px solid ${theme.palette.divider}`,
-	},
-	toolbar: {
-		flexWrap: 'wrap',
-	},
-	toolbarTitle: {
-		flexGrow: 1,
-	},
-	link: {
-		margin: theme.spacing(1, 1.5),
+	icon: {
+		marginRight: theme.spacing(2),
 	},
 	heroContent: {
+		backgroundColor: theme.palette.background.paper,
 		padding: theme.spacing(8, 0, 6),
 	},
-	cardHeader: {
-		backgroundColor: theme.palette.grey[200],
+	heroButtons: {
+		marginTop: theme.spacing(4),
 	},
-	cardPricing: {
+	cardGrid: {
+		paddingTop: theme.spacing(8),
+		paddingBottom: theme.spacing(8),
+	},
+	card: {
+		height: '100%',
 		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'baseline',
-		marginBottom: theme.spacing(2),
+		flexDirection: 'column',
 	},
-	img: {
-		height: 400,
-		display: 'block',
-		overflow: 'hidden',
-		width: '1020px',
+	cardMedia: {
+		paddingTop: '56.25%', // 16:9
+	},
+	cardContent: {
+		flexGrow: 1,
 	},
 	footer: {
-		borderTop: `1px solid ${theme.palette.divider}`,
-		marginTop: theme.spacing(8),
-		paddingTop: theme.spacing(3),
-		paddingBottom: theme.spacing(3),
-		[theme.breakpoints.up('sm')]: {
-			paddingTop: theme.spacing(6),
-			paddingBottom: theme.spacing(6),
-		},
+		backgroundColor: theme.palette.background.paper,
+		padding: theme.spacing(6),
 	},
 }));
 
-const tiers = [
-	{
-		title: 'Basic',
-		price: '500',
-		description: ['10 users included', '2 GB of storage', 'Help center access', 'Email support'],
-		buttonText: 'Get Started',
-		buttonVariant: 'outlined',
-		backgroundcolor: '#e55242',
-	},
-	{
-		title: 'Premium',
-		subheader: 'Most popular',
-		price: '1500',
-		description: ['20 users included', '10 GB of storage', 'Help center access', 'Priority email support'],
-		buttonText: 'Get started',
-		buttonVariant: 'outlined',
-		backgroundcolor: '#09aa65',
-	},
-	{
-		title: 'Enterprize',
-		price: '1000',
-		description: ['50 users included', '30 GB of storage', 'Help center access', 'Phone & email support'],
-		buttonText: 'Get Started',
-		buttonVariant: 'outlined',
-		backgroundcolor: '#0999cf',
-	},
-];
-const footers = [
-	{
-		title: 'Company',
-		description: ['Team', 'History', 'Contact us', 'Locations'],
-	},
-	{
-		title: 'Features',
-		description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
-	},
-	{
-		title: 'Resources',
-		description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-	},
-	{
-		title: 'Legal',
-		description: ['Privacy policy', 'Terms of use'],
-	},
-];
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Main() {
 	const classes = useStyles();
 
 	return (
 		<React.Fragment>
-			<Container>
-				{/* <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-					Pricing
-				</Typography>
-				<Typography variant="h5" align="center" color="textSecondary" component="p">
-					Quickly build an effective pricing table for your potential customers with this layout. It&apos;s
-					built with default Material-UI components with little customization.
-				</Typography> */}
-				<img src={clientSideBG1} alt="clientSideBG1" className={classes.img} />
-			</Container>
-			{/* End hero unit */}
-			<Container maxWidth="md" component="main">
-				<Grid container spacing={3} alignItems="flex-end">
-					{tiers.map(tier => (
-						// Enterprise card is full width at sm breakpoint
-						<Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-							<Card>
-								<CardHeader
-									subheader={tier.subheader}
-									title={tier.title}
-									titleTypographyProps={{ align: 'center' }}
-									subheaderTypographyProps={{ align: 'center' }}
-									action={tier.title === 'Standard' ? <StarIcon /> : null}
-									style={{ backgroundColor: tier.backgroundcolor, color: 'white' }}
-								/>
-								<CardContent>
-									<div className={classes.cardPricing}>
-										<Typography component="h2" variant="h3" color="textPrimary">
-											${tier.price}
-										</Typography>
-										<Typography variant="h6" color="textSecondary">
-											/year
-										</Typography>
-									</div>
-									<ul>
-										{tier.description.map(line => (
-											<Typography component="li" variant="subtitle1" align="center" key={line}>
-												{line}
-											</Typography>
-										))}
-									</ul>
-								</CardContent>
-								<CardActions>
-									<Button fullWidth variant={tier.buttonVariant} color="primary">
-										{tier.buttonText}
+			<CssBaseline />
+			<AppBar position="relative">
+				<Toolbar>
+					<CameraIcon className={classes.icon} />
+					<Typography variant="h6" color="inherit" noWrap>
+						Album layout
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<main>
+				{/* Hero unit */}
+				<div className={classes.heroContent}>
+					<Container maxWidth="sm">
+						<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+							Album layout
+						</Typography>
+						<Typography variant="h5" align="center" color="textSecondary" paragraph>
+							Something short and leading about the collection below—its contents, the creator, etc. Make
+							it short and sweet, but not too short so folks don&apos;t simply skip over it entirely.
+						</Typography>
+						<div className={classes.heroButtons}>
+							<Grid container spacing={2} justify="center">
+								<Grid item>
+									<Button variant="contained" color="primary">
+										Main call to action
 									</Button>
-								</CardActions>
-							</Card>
-						</Grid>
-					))}
-				</Grid>
-			</Container>
+								</Grid>
+								<Grid item>
+									<Button variant="outlined" color="primary">
+										Secondary action
+									</Button>
+								</Grid>
+							</Grid>
+						</div>
+					</Container>
+				</div>
+				<Container className={classes.cardGrid} maxWidth="md">
+					{/* End hero unit */}
+					<Grid container spacing={4}>
+						{cards.map(card => (
+							<Grid item key={card} xs={12} sm={6} md={4}>
+								<Card className={classes.card}>
+									<CardMedia
+										className={classes.cardMedia}
+										image="https://source.unsplash.com/random"
+										title="Image title"
+									/>
+									<CardContent className={classes.cardContent}>
+										<Typography gutterBottom variant="h5" component="h2">
+											Heading
+										</Typography>
+										<Typography>
+											This is a media card. You can use this section to describe the content.
+										</Typography>
+									</CardContent>
+									<CardActions>
+										<Button size="small" color="primary">
+											View
+										</Button>
+										<Button size="small" color="primary">
+											Edit
+										</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
+					</Grid>
+				</Container>
+			</main>
 			{/* Footer */}
-			<Container maxWidth="md" component="footer" className={classes.footer}>
-				<Grid container spacing={4} justify="space-evenly">
-					{footers.map(footer => (
-						<Grid item xs={6} sm={3} key={footer.title}>
-							<Typography variant="h6" color="textPrimary" gutterBottom>
-								{footer.title}
-							</Typography>
-							<ul>
-								{footer.description.map(item => (
-									<li key={item}>
-										<Link href="#" variant="subtitle1" color="textSecondary">
-											{item}
-										</Link>
-									</li>
-								))}
-							</ul>
-						</Grid>
-					))}
-				</Grid>
-			</Container>
+			<footer className={classes.footer}>
+				<Typography variant="h6" align="center" gutterBottom>
+					Footer
+				</Typography>
+				<Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+					Something here to give the footer a purpose!
+				</Typography>
+				<MadeWithLove />
+			</footer>
 			{/* End footer */}
 		</React.Fragment>
 	);
